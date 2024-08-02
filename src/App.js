6085cc3bc400c22f37978d7b928562e53395d62c
abugeek <abdullohabdupattaev2002@gmail.com>
2024-08-02 23:28:20 +0500

    fix meta tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import TermsConditions from './pages/TermsConditions';
-import './styles/global.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+
+function usePageTitle(title) {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = title;
+  }, [location, title]);
+}
+
+const Home = () => {
+  usePageTitle('Bindle - Home');
+  return <div>Home Page</div>;
+};
+
+const PrivacyPolicy = () => {
+  usePageTitle('Bindle - Privacy Policy');
+  return <div>Privacy Policy Page</div>;
+};
 
 function App() {
   return (
@@ -11,7 +25,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/terms-conditions" element={<TermsConditions />} />
+        {/* Add other routes here */}
       </Routes>
     </Router>
   );
